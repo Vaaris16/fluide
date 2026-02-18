601ddf782354afb79c556e94461c696523d72b9f
Vaaris Chitkara <vaarischitkara16@gmail.com>
2026-02-18 17:31:02 +0530

    added better formatting

--- src/packages/tailwindcss_package/get_vite_conf.rs
+++ src/packages/tailwindcss_package/get_vite_conf.rs
@@ -10,7 +10,7 @@ pub fn get_vite_conf() -> Result<PathBuf, TailwindcssErrors> {
     for file in vite_conf_file {
         let path = cd.join(file);
         if path.exists() {
-            print!("found vite config");
+            print!("found vite config\n\n");
             return Ok(path);
         }
     }

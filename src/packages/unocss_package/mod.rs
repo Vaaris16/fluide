b08593e6d7d26ecd6f9ba20e1a4ceb243e7b8637
Vaaris Chitkara <vaarischitkara16@gmail.com>
2026-02-23 20:55:19 +0530

    minor error in unocss mod.rs

--- src/packages/unocss_package/mod.rs
+++ src/packages/unocss_package/mod.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 use crate::packages;
 
 pub fn setup_unocss(cd: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
-    let _ = packages::utils::npm::npm_command("npm,install,-d,unocss");
+    let _ = packages::utils::npm::npm_command("npm,install,-D,unocss");
     let path = packages::utils::vite_setup::get_vite_conf(&cd)?;
     packages::utils::edit_vite_conf::edit_vite_conf(
         &path,
